cb72380910221912b76172b0d1c9781741c61dc9
Bhardwaj Chaudhary <bhardwaj94@Bhardwajs-MacBook-Air.local>
2024-07-23 17:25:39 +0530

    path fix-11

--- .github/workflows/updatePRDescription.js
+++ .github/workflows/updatePRDescription.js
@@ -2,27 +2,30 @@
     const { Octokit } = await import("@octokit/core");
     const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
     const title = process.env.PR_TITLE;
+    const owner = process.env.OWNER;
+    const repo = process.env.REPO;
+    const pull_number = process.env.PR_NUMBER;
     // console.log("title==>>",title);
     const regex = /\[(.*-)?([^-]+)-([^\]]+)\]/;
     const match = title.match(regex);
-    console.log("match==>>",match);
+    console.log("match==>>", match);
     if (match) {
         const combinedString = `${match[2]}-${match[3]}`;
-        console.log("combinedString==>>",combinedString);
+        console.log("combinedString==>>", combinedString);
         octokit.request("GET /repos/{owner}/{repo}/pulls/{pull_number}", {
-            owner: github.repository_owner,
-            repo: github.event.repository.name,
-            pull_number: process.env.PR_NUMBER
+            owner,
+            repo,
+            pull_number
         }).then(({ data }) => {
             let description = data.body.replace('GEC-XXX', combinedString);
-        console.log("description==>>",description);
+            console.log("description==>>", description);
             return octokit.request("PATCH /repos/{owner}/{repo}/pulls/{pull_number}", {
-                owner: github.repository_owner,
-                repo: github.event.repository.name,
-                pull_number: process.env.PR_NUMBER,
+                owner,
+                repo,
+                pull_number,
                 body: description
             })
-        }).then(() => console.log("successful!!!")).catch(err => console.error("failed",err))
+        }).then(() => console.log("successful!!!")).catch(err => console.error("failed", err))
     } else {
         console.log("No match")
     }
