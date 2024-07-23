b53f2638cee3fe2eede4791bbea3e3af0b0f9b2b
Bhardwaj Chaudhary <bhardwaj94@Bhardwajs-MacBook-Air.local>
2024-07-23 17:00:34 +0530

    path fix-6

--- .github/workflows/updatePRDescription.js
+++ .github/workflows/updatePRDescription.js
@@ -1,23 +1,25 @@
-import { Octokit } from "@octokit/core";
-const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
-const title = process.env.PR_TITLE;
-const regex = /\[(.*-)?([^-]+)-([^\]]+)\]/;
-const match = title.match(regex);
-if (match) {
-    const combinedString = `${match[2]}-${match[3]}`;
-    octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}', {
-        owner: '${{ github.repository_owner }}',
-        repo: '${{ github.event.repository.name }}',
-        pull_number: process.env.PR_NUMBER
-    }).then(({ data }) => {
-        let description = data.body.replace('GEC-XXX', combinedString);
-        return octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}', {
+(async () => {
+    const { Octokit } = await import("@octokit/core");
+    const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+    const title = process.env.PR_TITLE;
+    const regex = /\[(.*-)?([^-]+)-([^\]]+)\]/;
+    const match = title.match(regex);
+    if (match) {
+        const combinedString = `${match[2]}-${match[3]}`;
+        octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}', {
             owner: '${{ github.repository_owner }}',
             repo: '${{ github.event.repository.name }}',
-            pull_number: process.env.PR_NUMBER,
-            body: description
-        })
-    }).then(() => console.log("successful!!!")).catch(err => console.error("failed"))
-} else {
-    console.log("No match")
-}
+            pull_number: process.env.PR_NUMBER
+        }).then(({ data }) => {
+            let description = data.body.replace('GEC-XXX', combinedString);
+            return octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}', {
+                owner: '${{ github.repository_owner }}',
+                repo: '${{ github.event.repository.name }}',
+                pull_number: process.env.PR_NUMBER,
+                body: description
+            })
+        }).then(() => console.log("successful!!!")).catch(err => console.error("failed"))
+    } else {
+        console.log("No match")
+    }
+})();
